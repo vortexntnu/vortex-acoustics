b122f233cfa149bc7d4d70fcc6b88128d03638af
Øystein Solbø <oyssolbo@stud.ntnu.no>
2020-12-26 16:17:35 +0100

    Changes to the filter and filter-coefficients. Included filterdesign from matlab
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- Include/DSP_constants.h
+++ Include/DSP_constants.h
@@ -80,65 +80,77 @@ const float32_t SAMPLE_TIME = (float32_t) 1 / SAMPLE_FREQUENCY;
  * 
  * @warning Must have abs(filter) < 1 to prevent overflow
  * 
- * @warning The sign of the filter-coefficients could possible
- * be wrong. Requires further testing to detect if the filter 
- * is stable/unstable with the given coefficients, as the 
- * documentation of the CMSISI-library is lacking at times...
- * 
  * @warning Filter coefficients are represented as fractional 
- * values and restricted to lie in the range [-1 +1)
+ * values and restricted to lie in the range [-1, +1)
  * 
- * @warning The calculations for the filter should be automated
- * such that the parameters changes according to the desired 
- * frequencies. The filter's parameters are calculated using
+ * @warning The filter is designed with the filterdesigner 
+ * in MATLAB, using
  *      Fs = 150000     Sampling frequency
  *      f0 = 30000      Passband center
  *      f1 = 15000      Lower cut-off frequency
  *      f2 = 45000      Upper cut-off frequency
  *      Q  = 1          Quality-factor
  *      K  = 1          Gain 
- * by using the calculator found at 
- * https://www.earlevel.com/main/2013/10/13/biquad-calculator-v2/
  * 
- * The calculator reports the values as {a0, a1, a2, b1, b2}, instead
- * of {b0, b1, b2, a1, a2}. As we are using a DF1 IIR-filter, the filter's 
- * transferfunction is given as 
+ * The filter's transferfunction is given as 
  *      H(z) = B(z) * 1 / A(z)
  * where
- *      B(z) = b0 + b1 * z^(-1) + b2 * z^(-2)
- *      A(z) = 1 + a1 * z^(-1) + a2 * z^(-2)
- * It is assumed that the calculator has swapped names between the denominator 
- * and the numerator. Otherwise, the filter coefficients would not
- * be possible given a DF1 IIR-filter with the first coefficient
- * in the denominator trivially assigned to 1. 
- * This must nonetheless be tested in the future
+ *      B(z) = b0 + b1 * z^(-1) + b2 * z^(-2) + b3 * z^(-3) + ...
+ *      A(z) = 1 + a1 * z^(-1) + a2 * z^(-2) + a3 * z^(-3) + ...
+ * 
+ * NOTE: Since the function uses a biquad-filter, every filter with order > 2 is split 
+ * into multiple serial second-order filters, leaving at most one filter with order = 1 
+ * 
+ * Example:
+ *      4th order filter with the original filter function given as
+ *      H(z) = B(z)/A(z) with
+ *
+ *          B(z) = b0 + b1 * z^(-1) + b2 * z^(-2) + b3 * z^(-3) + b4 * z^(-4)
+ *          A(z) = 1 + a1 * z^(-1) + a2 * z^(-2) + a3 * z^(-3) + a4 * z^(-4)
+ *    
+ *      is instead given as H(z) = B_1(z)/A_1(z) * B_2(z)/A_2(z), where
+ *      H_1(z) = B_1(z)/A_1(z)
+ * 
+ *          B_1(z) = b10 + b11 * z^(-1) + b12 * z^(-2)
+ *          A_1(z) = 1 + a11 * z^(-1) + a12 * z^(-2)
+ * 
+ *      and equivalent for B_2(z) and A_2(z)
  * 
  * NOTE: For more information, see 
  * https://arm-software.github.io/CMSIS_5/DSP/html/group__BiquadCascadeDF1__32x64.html
  * 
- * NOTE: The function void TESTING::test_filter_coefficients(void)
- * is under development for testing the filter
+ * NOTE: The MATLAB-script used to test the filter can be found under Resource/MATLAB
  * 
  * @param num_stages            Number of second order cascade-filters. Determines the
  *                              filter order. Order = 2 * num_stages
  * 
- * @param state_coefficients    Initial values for x[n-1], x[n-2], y[n-1] and y[n-2]
+ * @param state_coefficients    Initial values for x[n-1], x[n-2], ..., x[n-(2*num_stages)]
+ *                              y[n-1], y[n-2], ..., y[n-(2*num_stages)]
  * 
- * @param filter_coefficients   Filter coefficients given as {b0, b1, b2, a1, a2}
+ * @param filter_coefficients   Filter coefficients given as {b10, b11, b12, a11, a12
+ *                              b20, b21, b22, a21, a22, ...}
  * 
  * @param IIR_filter            A struct describing a biquad DF1 IIR filter
+ * 
+ * @param post_shift            Scaling-factor to keep all filter coefficients in the range
+ *                              [-1, +1). Scales all of the filter coefficients! 
+ *                              IMPORTANT: post_shift is a factor of 2, so post_shift = 1 
+ *                              scales by 2
  */
-const uint32_t num_stages = 1;
-const float32_t state_coefficients[4*num_stages] = {0.0, 0.0, 
-        0.0, 0.0};
-//const float32_t filter_coefficients[5*num_stages] = {0.32227662, 
-//        0.0, -0.32227662, -0.41885608, 0.35544676};
-// Filter coefficients recommended by MATLAB (Chebychev T1 PB IIR)
-const float32_t filter_coefficients[5*num_stages] = {0.58810799,
-        0, -0.58810799, -0.31465750, -0.17621597}; 
+const uint32_t num_stages = 2;
+const float32_t state_coefficients[4 * num_stages] = {0.0, 0.0, 
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+const float32_t filter_coefficients[5 * num_stages] = {
+        0.22725101, 0.0, -0.22725101,   /* Numerator filter 1 */ 
+        -0.58226175, 0.26593912,        /* Denominator filter 1 */
+        0.45450202, 0.0, -0.45450202,   /* Numerator filter 2 */
+        0.25944456, 0.36815892}         /* Denominator filter 2 */
+const uint8_t post_shift = 1; 
 const arm_biquad_casd_df1_inst_f32 IIR_filter = {
-    .numStages = num_stages, .pState = &filter_coefficients[0],
-    .pCoeffs = &filter_coefficients[0]};
+    .numStages = num_stages, 
+    .pState = &filter_coefficients[0],
+    .pCoeffs = &filter_coefficients[0], 
+    .postShift = post_shift};
 
 } /* namespace DSP_CONSTANTS */
 
