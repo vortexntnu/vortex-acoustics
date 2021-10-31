ff7c776ac285dba719f34cfc8c7d66b1c67fafdb
Adrian Haugland <adhaugland@gmail.com>
2021-10-31 14:28:51 +0100

    Formatting analze_data.py

--- Resource/PYTHON/multilateration/analyze_data.py
+++ Resource/PYTHON/multilateration/analyze_data.py
@@ -2,24 +2,22 @@ import numpy as np
 from scipy import signal
 
 
-
 # Functions for analyzing the data
 
 
-# @brief Function to find the index and the maximum abs value in an
-# array. The function returns the index and the maximum value 
-# indirectly as references
-
-# If @p array_length is invalid (negative), the function sets both
-# @p max_val and @p idx to -1
-# @param data_array The array to find the maximum of
-# @param array_length The length of the array
-# @param idx The index containing the maximum abs value
-# @param max_val Max absolute value
-
-
 def calculate_xcorr_lag_array(data_array):
-    # Getting the values from the arrays
+    """Crosscorrelates the filtered data arrays and returns an array which
+    gives the number of samples between the measured signals at hyd1, hyd2, hyd3.
+
+    Args:
+        data_array: Array of filtered data from the hydrophones
+
+    Returns:
+        A numpy.array containing calculated cross-correlated lag between the hydrophones:
+        port_starboard_lag, port_stern_lag, starboard_stern_lag.
+    """
+
+    # Getting the values from data arrays
     data_port = data_array[0]
     data_starboard = data_array[1]
     data_stern = data_array[2]
@@ -27,16 +25,23 @@ def calculate_xcorr_lag_array(data_array):
     # Crosscorrelating the data
     # https://docs.scipy.org/doc/scipy/reference/reference/generated/scipy.signal.correlation_lags.html#scipy.signal.correlation_lags
     cross_corr_port_starboard = signal.correlate(data_port, data_starboard, mode="full")
-    port_starboard_lags = signal.correlation_lags(data_port.size, data_starboard.size, mode="full")
+    port_starboard_lags = signal.correlation_lags(
+        data_port.size, data_starboard.size, mode="full"
+    )
     port_starboard_lag = port_starboard_lags[np.argmax(cross_corr_port_starboard)]
 
     cross_corr_port_stern = signal.correlate(data_port, data_stern, mode="full")
-    port_stern_lags = signal.correlation_lags(data_port.size, data_stern.size, mode="full")
+    port_stern_lags = signal.correlation_lags(
+        data_port.size, data_stern.size, mode="full"
+    )
     port_stern_lag = port_stern_lags[np.argmax(cross_corr_port_stern)]
-    
-    cross_corr_starboard_stern = signal.correlate(data_starboard, data_stern, mode="full")
-    starboard_stern_lags = signal.correlation_lags(data_starboard.size, data_stern.size, mode="full")
-    starboard_stern_lag = starboard_stern_lags[np.argmax(cross_corr_starboard_stern)]
-    
-    return port_starboard_lag, port_stern_lag, starboard_stern_lag
 
+    cross_corr_starboard_stern = signal.correlate(
+        data_starboard, data_stern, mode="full"
+    )
+    starboard_stern_lags = signal.correlation_lags(
+        data_starboard.size, data_stern.size, mode="full"
+    )
+    starboard_stern_lag = starboard_stern_lags[np.argmax(cross_corr_starboard_stern)]
+
+    return port_starboard_lag, port_stern_lag, starboard_stern_lag
