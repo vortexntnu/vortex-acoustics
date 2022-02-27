a7c9b71d25e554787ad80c9f5b0cf6e39ffb0f88
Hannah CL <hannahchaplin@hotmail.no>
2022-02-27 17:05:06 +0100

    Run simple test in natic and teensy41

--- cpp/teensy/test/test_correlation.cpp
+++ cpp/teensy/test/test_correlation.cpp
@@ -3,13 +3,10 @@
 #include "correlation.h"
 
 
-void test_nimportequoi(){
-    TEST_ASSERT_TRUE(nimportequoi); 
-
-}
 
 int main(int argc, char **argv) {
+
     UNITY_BEGIN(); 
-    RUN_TEST(test_nimportequoi); 
+    TEST_ASSERT_EQUAL_INT(3, nimportequoi(3)); 
     UNITY_END(); 
 }
