8d8531272f3d9905a4a838abc80d74ca327762d8
Hannah Laugaland <hannahchaplin@hotmail.no>
2022-03-17 17:01:42 +0100

    Remove debugging code from main

--- cpp/teensy/src/main.cpp
+++ cpp/teensy/src/main.cpp
@@ -2,59 +2,8 @@
 #include "arm_math.h"
 #include "stdio.h"
 #include "algorithm"
-//#include "correlation.h"
-
-void demo_correlation(){
-    Serial.begin(9660);
-    while (!Serial){} 
-    Serial.println("Serial connected"); 
-
-    float32_t arr1[] = {3.0, 5.2, 5.2, 6.7, 8.9, 2.0, 6.7, 6.7, 5.7, 0.0, 0.0}; 
-    float32_t arr2[] = {0.0, 0.0, 3.0, 5.2, 5.2, 6.7, 8.9, 2.0, 6.7, 6.7, 5.7};  
-
-    uint32_t computed_size = sizeof(arr1)/sizeof(arr1[0]);
-    Serial.printf("Array size is: %i \n", sizeof(arr1)); 
-    Serial.printf("Type size is: %i \n", sizeof(arr1[0])); 
-    Serial.printf("Computed input size is: %i \n", computed_size); 
-
-    uint32_t size =  2 * 11 - 1; 
-    float32_t result_from_teensy[size]; 
-    arm_correlate_f32(arr1, 11, arr2, 11, result_from_teensy); 
-    Serial.println("Post Correlation");
-
-
-    for (uint32_t i = 0; i < size; i++){ 
-        Serial.printf("%.3f, ", result_from_teensy[i]);
-    }
-    Serial.println("");
-
-
-    while(true){
-        Serial.printf("Loop"); 
-        delay(1000); 
-    }  
-}
-
-
-
-void print_array(float32_t sig1[], uint32_t length_sig1){
-    Serial.print("Array: "); 
-    for (int i = 0; i < length_sig1; i++){
-        Serial.printf("%.1f ,", sig1[i]); 
-    }
-    Serial.println(""); Serial.println("");
-}
 
 
 int main(void) {
-    Serial.begin(9660);
-    while (!Serial){} 
-    Serial.println("Serial connected"); 
-
-
-    while(true){
-        Serial.printf("Loop"); 
-        delay(1000); 
-    }
-    return 0;
+    while(true){}
 }
