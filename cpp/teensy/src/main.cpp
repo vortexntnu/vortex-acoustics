f7b21d82b23754927412b1591bc0e5e79e346266
Hannah Laugaland <hannahchaplin@hotmail.no>
2022-03-20 13:41:58 +0100

    Add func for init matricies

--- cpp/teensy/src/main.cpp
+++ cpp/teensy/src/main.cpp
@@ -1,9 +1,18 @@
 #include "algorithm"
 #include "arm_math.h"
 #include "stdio.h"
-#include <Arduino.h>
+
+#include "algorithm"
+#include "../lib/multilateration/multilateration.h"
+
 
 int main(void) {
-    while (true) {
-    }
+    Serial.begin(9600); 
+    while(!Serial){}
+    Serial.println("Serial connected"); 
+
+    float32_t** A = init_A_matrix(); 
+    float32_t* B = init_B_matrix(); 
+
+    while(true){}
 }
