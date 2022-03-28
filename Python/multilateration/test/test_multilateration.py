8617c343ef3163dd63aa7cd1617ab38620fc726c
hannahcl <85459191+hannahcl@users.noreply.github.com>
2022-03-28 17:25:31 +0200

    Add comment describing purpose of python test
    Co-authored-by: Nicholas Bresina <90988086+nicbres@users.noreply.github.com>

--- Python/multilateration/test/test_multilateration.py
+++ Python/multilateration/test/test_multilateration.py
@@ -135,6 +135,10 @@ def test_trilateration_algorithm():
 
 
 def test_compare_with_teensy():
+    """Provides a reference for the code on the Teensy to compare against.
+    
+    The estimates that are generated within this test should be the same if run on the Teensy.
+    """
 
     tdoa_sample_array = np.empty(4, int)
     tdoa_sample_array[0] = 1
