8c37ad5f9ad486d3ba276616727aa9f3cbcea0de
Hannah Laugaland <hannahchaplin@hotmail.no>
2022-04-10 09:36:06 +0200

    add test for integrate new measurement

--- Python/accumulate_position_estimates/test/test_accumulate_position_estimates.py
+++ Python/accumulate_position_estimates/test/test_accumulate_position_estimates.py
@@ -1,4 +1,5 @@
 import logging
+from statistics import mean
 
 #import matplotlib.pyplot as plt
 import numpy as np
@@ -8,8 +9,10 @@ from multilateration import multilateration as ml
 from utilities import tdoa
 from utilities.drone import Drone
 
+import accumulate_position_estimates as acc
 
-def generate_estimate(
+
+def generate_estimates(
     source_position: sg_pos.Position,
     hydrophone_positions: np.array,
     sample_frequency: float = 100000,
@@ -19,10 +22,11 @@ def generate_estimate(
         source_position: for useful plots make sure source positidebugon is in positive octant
         hydrophone_positions: give hydrophone positions relative to origo (0,0,0)
     """
+
+    estimates = []
     drone = Drone(
         hydrophone_positions=hydrophone_positions,
     )
-
 
     xs = np.linspace(0, source_position.x, 6)[:-1]
     ys = np.linspace(0, source_position.y, 6)[:-1]
@@ -48,8 +52,63 @@ def generate_estimate(
                     sample_frequency=sample_frequency,
                 )
 
-                estimate = sg_pos.Position(
+                estimate = acc.Position(
                     x=res_x,
                     y=res_y,
                     z=res_z,
                 )
+
+                estimates.append(estimate)
+
+    return estimates
+
+                
+
+def test_integrate_new_measurement(): 
+
+    source_position = sg_pos.Position(10, 10, 10)
+
+    hydrophone_positions = np.array(
+    [
+        sg_pos.Position(
+            x=-0.11,
+            y=0.31,
+            z=0.1,
+        ),
+        sg_pos.Position(
+            x=0.11,
+            y=0.31,
+            z=0.1,
+        ),
+        sg_pos.Position(
+            x=0.0,
+            y=-0.24,
+            z=0.0,
+        ),
+        sg_pos.Position(
+            x=0.5,
+            y=-0.1,
+            z=0.4,
+        ),
+        sg_pos.Position(
+            x=0.4,
+            y=0.0,
+            z=-0.4,
+        ),
+    ])
+    sampling_frequency = 320
+
+    estimates = generate_estimates(source_position, hydrophone_positions, sampling_frequency)
+
+    computed_pinger_position = acc.PositionEstimate()
+
+    for estimate in estimates:
+        computed_pinger_position.integrate_new_measurement(estimate)
+
+    tolerance = 0.5
+
+    assert tolerance < abs(source_position.x - computed_pinger_position.mean.x)
+    assert tolerance < abs(source_position.y - computed_pinger_position.mean.y)
+    assert tolerance < abs(source_position.z - computed_pinger_position.mean.z)
+
+
