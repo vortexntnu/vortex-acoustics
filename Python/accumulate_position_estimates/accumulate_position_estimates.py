3e7afd455ea30828c60a3435091618cbbcebb02c
Hannah Laugaland <hannahchaplin@hotmail.no>
2022-04-10 18:32:21 +0200

    Use somewhat realistic test parameters

--- Python/accumulate_position_estimates/accumulate_position_estimates.py
+++ Python/accumulate_position_estimates/accumulate_position_estimates.py
@@ -1,73 +1,46 @@
-"""
-TODO: 
-    - store measurments
-        - np array with fixed size equal worst case
-        - use python list 
-
-    - Detect change in pinger position
-        - can check what task is being worked on (check state)
-        - can detect significant change in mean
-        - both? 
-
-    - Test
-        - mean corresponds to initial position
-        - sampling frequency equals 320, 427
-        - decrease tolerance
-        - increasing distance to source positions
-        - detect change in pinger pos
-            - change fsm bool before and after
-            - find good number for n_last_elem
-
-"""
-
 from dataclasses import dataclass
-import numpy as np
-
-
-@dataclass 
+@dataclass
 class Position:
     x: float = 0
     y: float = 0
     z: float = 0
 
+pool_size = Position(25, 12.5, 5)
 
 class PositionEstimate: 
-    mean = Position(0,0,0)
-    variance = Position(0,0,0)
-    measurements = [[],[],[]]
-    recent_measurements = [[],[],[]]
-    
-    n_last_elements = 5                    # make init func
-    distance_threshold = Position(3,3,3)  #check competition spec - 22x12
 
-    def __init__(self, n_last_elem, distance_threshold) -> None:
-        self.n_last_elements = n_last_elem
-        self.distance_threshold = distance_threshold
+    def __init__(self, m_last_elem, distance_threshold) -> None:
+        self.mean = Position(0,0,0)
+        self.variance = Position(0,0,0)
+        self.measurements = [[],[],[]]
 
+        self.m_last_elements = m_last_elem           
+        self.distance_threshold = distance_threshold 
 
     def integrate_new_measurement(self, measurment: list):
-        for i in range (3):
-            self.measurements[i].append(measurment[i])
- 
+
+        if measurment[0] < pool_size.x and measurment[1] < pool_size.y and measurment[2] < pool_size.z:  
+            self.measurements[0].append(measurment[0])
+            self.measurements[1].append(measurment[1])
+            self.measurements[2].append(measurment[2])
+
+            n = len(self.measurements[0])
+
+            self.mean.x = sum(self.measurements[0])/n
+            self.mean.y = sum(self.measurements[1])/n
+            self.mean.z = sum(self.measurements[2])/n
+
+            self.variance.x = sum((x-self.mean.x)**2 for x in self.measurements[0]) / n
+            self.variance.y = sum((y-self.mean.y)**2 for y in self.measurements[1]) / n
+            self.variance.z = sum((z-self.mean.z)**2 for z in self.measurements[2]) / n
+
+
+    def detect_change_in_pinger_pos(self, searching_for_second_pinger : bool):
 
         n = len(self.measurements[0])
+        m = self.m_last_elements
 
-        self.mean.x = sum(self.measurements[0])/n
-        self.mean.y = sum(self.measurements[1])/n
-        self.mean.z = sum(self.measurements[2])/n
-
-        self.variance.x = sum((x-self.mean.x)**2 for x in self.measurements[0]) / n
-        self.variance.y = sum((y-self.mean.y)**2 for y in self.measurements[1]) / n
-        self.variance.z = sum((z-self.mean.z)**2 for z in self.measurements[2]) / n
-
-    def detect_change_in_pinger_pos(
-        self, 
-        searching_for_second_pinger : bool
-        ):
-
-        if searching_for_second_pinger:
-            n = len(self.measurements[0])
-            m = self.n_last_elements
+        if searching_for_second_pinger and n>m*2:
 
             prev_mean_x = sum(self.measurements[0][:n-m])/(n-m)
             prev_mean_y = sum(self.measurements[1][:n-m])/(n-m)
@@ -80,9 +53,13 @@ class PositionEstimate:
             if self.distance_threshold.x < abs(prev_mean_x-current_mean_x):
                 if self.distance_threshold.y < abs(prev_mean_y-current_mean_y):
                     if self.distance_threshold.z < abs(prev_mean_z - current_mean_z):
-                        print("CHANGE IN POS")
-                        for i in range (3):
-                            self.measurements[i] = self.measurements[i][n-m:]
+                        self.measurements[0] = self.measurements[0][n-m:]
+                        self.measurements[1] = self.measurements[1][n-m:]
+                        self.measurements[2] = self.measurements[2][n-m:]
+
+
+                        
+                        
     
 
 
--- Python/accumulate_position_estimates/test/test_accumulate_position_estimates.py
+++ Python/accumulate_position_estimates/test/test_accumulate_position_estimates.py
@@ -10,11 +10,11 @@ import accumulate_position_estimates as acc
 def generate_estimates(
     source_position: sg_pos.Position,
     hydrophone_positions: np.array,
-    sample_frequency: float = 100000, #[kHz] og [Hz] ? 
+    sample_frequency: float = 100000, 
 ):
     """
     Args:
-        source_position: for useful plots make sure source positidebugon is in positive octant
+        source_position: make sure source positidebugon is in positive octant
         hydrophone_positions: give hydrophone positions relative to origo (0,0,0)
     """
 
@@ -23,9 +23,10 @@ def generate_estimates(
         hydrophone_positions=hydrophone_positions,
     )
 
+    # With these values 150 estimates are generated, which will take about 5 min. 
     xs = np.linspace(0, source_position.x, 6)[:-1]
-    ys = np.linspace(0, source_position.y, 6)[:-1]
-    zs = np.linspace(0, source_position.z, 6)[:-1]
+    ys = np.linspace(0, source_position.y, 5)[:-1]
+    zs = np.linspace(0, source_position.z, 5)[:-1]
 
     for x in xs:
         for y in ys:
@@ -61,65 +62,17 @@ def check_result(
     print_mean_and_var = False,
     ):
 
-    tol_x = np.sqrt(computed_pinger_position.variance.x)
-    tol_y = np.sqrt(computed_pinger_position.variance.y)
-    tol_z = np.sqrt(computed_pinger_position.variance.z)
-
     if print_mean_and_var:
         print("Mean: ")
         print_pos(computed_pinger_position.mean)
         print("Variance: ")
         print_pos(computed_pinger_position.variance)
 
-    assert tol_x > abs(source_position.x - computed_pinger_position.mean.x)
-    assert tol_y > abs(source_position.y - computed_pinger_position.mean.y)
-    assert tol_z > abs(source_position.z - computed_pinger_position.mean.z)
 
-
-def test_integrate_new_measurement(): 
-
-    source_position = sg_pos.Position(6,5,4)
-
-    hydrophone_positions = np.array(
-    [
-        sg_pos.Position(
-            x=-0.11,
-            y=0.31,
-            z=0.1,
-        ),
-        sg_pos.Position(
-            x=0.11,
-            y=0.31,
-            z=0.1,
-        ),
-        sg_pos.Position(
-            x=0.0,
-            y=-0.24,
-            z=0.0,
-        ),
-        sg_pos.Position(
-            x=0.5,
-            y=-0.1,
-            z=0.4,
-        ),
-        sg_pos.Position(
-            x=0.4,
-            y=0.0,
-            z=-0.4,
-        ),
-    ])
-
-    sampling_frequency = 10000
-    n_last_elemetns = 5
-    distance_threshold = acc.Position(3,3,3)
-    computed_pinger_position = acc.PositionEstimate(n_last_elemetns, distance_threshold)
-
-    estimates = generate_estimates(source_position, hydrophone_positions, sampling_frequency)
-    for estimate in estimates:
-        computed_pinger_position.integrate_new_measurement(estimate)
-
-    check_result(source_position, computed_pinger_position, True)
-
+    #1.7 is based on the fact the perception should be able to recognise the torpedo task 3 meters away.
+    assert 1.7 > abs(source_position.x - computed_pinger_position.mean.x)
+    assert 1.7 > abs(source_position.y - computed_pinger_position.mean.y)
+    assert 1.7 > abs(source_position.z - computed_pinger_position.mean.z)
 
 
 def test_detect_change_in_pinger_pos():
@@ -153,14 +106,16 @@ def test_detect_change_in_pinger_pos():
         ),
     ])
 
-    sampling_frequency = 10000
-    n_last_elemetns = 5
-    distance_threshold = acc.Position(3,3,3)
-    computed_pinger_position = acc.PositionEstimate(n_last_elemetns, distance_threshold)
 
-    first_source_position = sg_pos.Position(5,5,5)
-    second_source_position = sg_pos.Position(1,1,1)
-    
+    # The first and second pinger position, as well as the distance threshold, 
+    # are based on the course layout figure from RoboSub handbook (January 2022).
+    first_source_position = sg_pos.Position(1.5,3.125,2)
+    second_source_position = sg_pos.Position(10,7.25,2)
+    distance_threshold = acc.Position(8, 3.0, 0)
+
+    sampling_frequency = 320*10e3 # [Hz]
+    m_last_elemetns = 30          # Lower the numer increases the chance of false alarm. Higher number the time it takes to detect change in position.
+    computed_pinger_position = acc.PositionEstimate(m_last_elemetns, distance_threshold)
 
     first_estimates = generate_estimates(first_source_position, hydrophone_positions, sampling_frequency)
 
@@ -168,15 +123,24 @@ def test_detect_change_in_pinger_pos():
         computed_pinger_position.integrate_new_measurement(estimate)
         computed_pinger_position.detect_change_in_pinger_pos(True)
 
-    check_result(first_source_position, computed_pinger_position)
+    check_result(first_source_position, computed_pinger_position, True)
 
     second_estimates = generate_estimates(second_source_position, hydrophone_positions, sampling_frequency)
 
-    for estimate in second_estimates[:7]:
+    print("\n\n new position \n\n")
+    for estimate in second_estimates: 
         computed_pinger_position.integrate_new_measurement(estimate)
         computed_pinger_position.detect_change_in_pinger_pos(True)
 
-    check_result(second_source_position, computed_pinger_position)
+    check_result(second_source_position, computed_pinger_position, True)
+
+
+
+
+
+
+
+
 
 
 
