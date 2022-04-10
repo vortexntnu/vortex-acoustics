278802dd8612091719544ceeba02fb51a2bb2981
Hannah Laugaland <hannahchaplin@hotmail.no>
2022-04-10 12:17:15 +0200

    Add func for detecting change in pinger pos

--- Python/accumulate_position_estimates/accumulate_position_estimates.py
+++ Python/accumulate_position_estimates/accumulate_position_estimates.py
@@ -4,11 +4,15 @@ TODO:
         - np array with fixed size equal worst case
         - use python list 
     - Detect change in pinger position
+        - can check what task is being worked on (check state)
+        - can detect significant change in mean
+        - both? 
+
     - Test
         - mean corresponds to initial position
-        - var decreases with many itterations
         - sampling frequency equals 320, 427
         - decrease tolerance
+        - many source positions
 
 """
 
@@ -27,6 +31,9 @@ class PositionEstimate:
     mean = Position(0,0,0)
     variance = Position(0,0,0)
     measurements = [[],[],[]]
+    recent_measurements = [[],[],[]]
+    n_last_elements = 5
+    distance_threshold = Position(5,5,5) #check competition spec
 
 
     def integrate_new_measurement(self, measurment: list):
@@ -43,6 +50,36 @@ class PositionEstimate:
         self.variance.x = sum((x-self.mean.x)**2 for x in self.measurements[0]) / n
         self.variance.y = sum((y-self.mean.y)**2 for y in self.measurements[1]) / n
         self.variance.z = sum((z-self.mean.z)**2 for z in self.measurements[2]) / n
+
+    def detect_change_in_pinger_pos(
+        self, 
+        searching_for_second_pinger : bool
+        ):
+
+        if searching_for_second_pinger:
+            n = len(self.measurements[0])
+            m = self.n_last_elements
+
+            prev_mean_x = sum(self.measurements[0][:n-m])/(n-m)
+            prev_mean_y = sum(self.measurements[1][:n-m])/(n-m)
+            prev_mean_z = sum(self.measurements[2][:n-m])/(n-m)
+
+            current_mean_x = sum(self.measurements[0][n-m:])/m
+            current_mean_y = sum(self.measurements[1][n-m:])/m
+            current_mean_z = sum(self.measurements[2][n-m:])/m
+
+            if self.distance_threshold.x < abs(prev_mean_x-current_mean_x):
+                if self.distance_threshold.y < abs(prev_mean_y-current_mean_y):
+                    if self.distance_threshold.z < abs(prev_mean_z - current_mean_z):
+                        for i in range (3):
+                            self.measurements[i] = self.measurements[i][n-m:]
+    
+
+
+
+        
+  
+            
 
         
 
