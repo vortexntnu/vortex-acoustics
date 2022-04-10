dc070befd2e562e159b2d03f4a1587d79a1d3562
Hannah Laugaland <hannahchaplin@hotmail.no>
2022-04-10 13:13:01 +0200

    Add test for detect change in pinger pos

--- Python/accumulate_position_estimates/accumulate_position_estimates.py
+++ Python/accumulate_position_estimates/accumulate_position_estimates.py
@@ -3,6 +3,7 @@ TODO:
     - store measurments
         - np array with fixed size equal worst case
         - use python list 
+
     - Detect change in pinger position
         - can check what task is being worked on (check state)
         - can detect significant change in mean
@@ -12,7 +13,10 @@ TODO:
         - mean corresponds to initial position
         - sampling frequency equals 320, 427
         - decrease tolerance
-        - many source positions
+        - increasing distance to source positions
+        - detect change in pinger pos
+            - change fsm bool before and after
+            - find good number for n_last_elem
 
 """
 
@@ -32,8 +36,13 @@ class PositionEstimate:
     variance = Position(0,0,0)
     measurements = [[],[],[]]
     recent_measurements = [[],[],[]]
-    n_last_elements = 5
-    distance_threshold = Position(5,5,5) #check competition spec
+    
+    n_last_elements = 5                    # make init func
+    distance_threshold = Position(3,3,3)  #check competition spec - 22x12
+
+    def __init__(self, n_last_elem, distance_threshold) -> None:
+        self.n_last_elements = n_last_elem
+        self.distance_threshold = distance_threshold
 
 
     def integrate_new_measurement(self, measurment: list):
@@ -71,6 +80,7 @@ class PositionEstimate:
             if self.distance_threshold.x < abs(prev_mean_x-current_mean_x):
                 if self.distance_threshold.y < abs(prev_mean_y-current_mean_y):
                     if self.distance_threshold.z < abs(prev_mean_z - current_mean_z):
+                        print("CHANGE IN POS")
                         for i in range (3):
                             self.measurements[i] = self.measurements[i][n-m:]
     
--- Python/accumulate_position_estimates/test/test_accumulate_position_estimates.py
+++ Python/accumulate_position_estimates/test/test_accumulate_position_estimates.py
@@ -1,18 +1,10 @@
-import logging
-from statistics import mean
-
-#import matplotlib.pyplot as plt
 import numpy as np
 import signal_generation.positioning as sg_pos
 from multilateration import multilateration as ml
-#from multilateration import parameters
 from utilities import tdoa
 from utilities.drone import Drone
 
 import accumulate_position_estimates as acc
-
-def print_pos(position: acc.Position):
-    print("x: ", position.x, "y: ", position.y, "z: ", position.z, "\n")
 
 
 def generate_estimates(
@@ -60,11 +52,33 @@ def generate_estimates(
 
     return estimates
 
-                
+def print_pos(position: acc.Position):
+    print("x: ", position.x, "y: ", position.y, "z: ", position.z, "\n") 
+
+def check_result(
+    source_position: acc.Position, 
+    computed_pinger_position: acc.Position, 
+    print_mean_and_var = False,
+    ):
+
+    tol_x = np.sqrt(computed_pinger_position.variance.x)
+    tol_y = np.sqrt(computed_pinger_position.variance.y)
+    tol_z = np.sqrt(computed_pinger_position.variance.z)
+
+    if print_mean_and_var:
+        print("Mean: ")
+        print_pos(computed_pinger_position.mean)
+        print("Variance: ")
+        print_pos(computed_pinger_position.variance)
+
+    assert tol_x > abs(source_position.x - computed_pinger_position.mean.x)
+    assert tol_y > abs(source_position.y - computed_pinger_position.mean.y)
+    assert tol_z > abs(source_position.z - computed_pinger_position.mean.z)
+
 
 def test_integrate_new_measurement(): 
 
-    source_position = sg_pos.Position(2,5,5)
+    source_position = sg_pos.Position(6,5,4)
 
     hydrophone_positions = np.array(
     [
@@ -96,25 +110,78 @@ def test_integrate_new_measurement():
     ])
 
     sampling_frequency = 10000
+    n_last_elemetns = 5
+    distance_threshold = acc.Position(3,3,3)
+    computed_pinger_position = acc.PositionEstimate(n_last_elemetns, distance_threshold)
 
     estimates = generate_estimates(source_position, hydrophone_positions, sampling_frequency)
-
-    computed_pinger_position = acc.PositionEstimate()
-
     for estimate in estimates:
         computed_pinger_position.integrate_new_measurement(estimate)
 
-    tol_x = np.sqrt(computed_pinger_position.variance.x)*1
-    tol_y = np.sqrt(computed_pinger_position.variance.y)*1
-    tol_z = np.sqrt(computed_pinger_position.variance.z)*1
+    check_result(source_position, computed_pinger_position, True)
 
-    print("Mean: ")
-    print_pos(computed_pinger_position.mean)
-    print("Variance: ")
-    print_pos(computed_pinger_position.variance)
 
-    assert tol_x > abs(source_position.x - computed_pinger_position.mean.x)
-    assert tol_y > abs(source_position.y - computed_pinger_position.mean.y)
-    assert tol_z > abs(source_position.z - computed_pinger_position.mean.z)
+
+def test_detect_change_in_pinger_pos():
+
+    hydrophone_positions = np.array(
+    [
+        sg_pos.Position(
+            x=-0.11,
+            y=0.31,
+            z=0.1,
+        ),
+        sg_pos.Position(
+            x=0.11,
+            y=0.31,
+            z=0.1,
+        ),
+        sg_pos.Position(
+            x=0.0,
+            y=-0.24,
+            z=0.0,
+        ),
+        sg_pos.Position(
+            x=0.5,
+            y=-0.1,
+            z=0.4,
+        ),
+        sg_pos.Position(
+            x=0.4,
+            y=0.0,
+            z=-0.4,
+        ),
+    ])
+
+    sampling_frequency = 10000
+    n_last_elemetns = 5
+    distance_threshold = acc.Position(3,3,3)
+    computed_pinger_position = acc.PositionEstimate(n_last_elemetns, distance_threshold)
+
+    first_source_position = sg_pos.Position(5,5,5)
+    second_source_position = sg_pos.Position(1,1,1)
+    
+
+    first_estimates = generate_estimates(first_source_position, hydrophone_positions, sampling_frequency)
+
+    for estimate in first_estimates:
+        computed_pinger_position.integrate_new_measurement(estimate)
+        computed_pinger_position.detect_change_in_pinger_pos(True)
+
+    check_result(first_source_position, computed_pinger_position)
+
+    second_estimates = generate_estimates(second_source_position, hydrophone_positions, sampling_frequency)
+
+    for estimate in second_estimates[:7]:
+        computed_pinger_position.integrate_new_measurement(estimate)
+        computed_pinger_position.detect_change_in_pinger_pos(True)
+
+    check_result(second_source_position, computed_pinger_position)
+
+
+
+
+
+
 
 
