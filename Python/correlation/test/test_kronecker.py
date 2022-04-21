7d414cea900e0e642d60423830eb2088cb46066d
Hannah Laugaland <hannahchaplin@hotmail.no>
2022-04-21 19:58:41 +0200

    Running, but gives asserting error

--- Python/correlation/test/test_kronecker.py
+++ Python/correlation/test/test_kronecker.py
@@ -8,10 +8,10 @@ def test_initialize():
     arr2 = [0.0, 0.0, 3.0, 5.2, 5.2, 6.7, 8.9, 2.0, 6.7, 6.7, 0.0, 0.0, 3.0, 5.2, 5.2, 6.7, 8.9, 2.0, 6.7, 6.7,0.0, 0.0, 3.0, 5.2, 5.2, 6.7, 8.9, 2.0, 6.7, 6.7, 2.0, 3.0, 2.0, 3.0, 8.9, 2.0, 6.7, 6.7, 2.0, 3.0, 2.0, 3.0]
 
     sdoa_estimation = kr.Kronecker(arr1, arr2)
-    #sdoa_estimation.iteration()
-    #sdoa = sdoa_estimation.final_computation()
+    sdoa_estimation.iteration()
+    sdoa = sdoa_estimation.final_computation()
 
-    #assert abs(sdoa) == 2
+    assert abs(sdoa) == 2
 
     
 
