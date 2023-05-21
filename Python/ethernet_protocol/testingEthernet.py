6de1fee4b2ee4dc39400c4923c1a868230d06514
Martynas <karate.martynas1@gamil.com>
2023-05-22 00:27:09 +0200

    Added a variable for clarifying value meaning

--- Python/ethernet_protocol/testingEthernet.py
+++ Python/ethernet_protocol/testingEthernet.py
@@ -13,6 +13,7 @@ import time
 
 HOST = "10.0.0.111"
 PORT = 8888  # (non-privileged ports are > 1023)
+MAX_PACKAGE_SIZE_RECEIVED = 65536
 
 # Socket setup
 address = (HOST, PORT)
@@ -22,15 +23,14 @@ clientSocket.settimeout(5) # Wait period before giving up on signal
 while(True):
     data = "Hello world".encode()  # Converts the string to bytes
     clientSocket.sendto(data, address) # Send data request
-    print("Data sent")
 
     try:
         #Read response from arduino
-        rec_data, addr = clientSocket.recvfrom(65536) 
+        rec_data, addr = clientSocket.recvfrom(MAX_PACKAGE_SIZE_RECEIVED) 
         messageReceived = rec_data.decode()
         print (f"Message from {addr}: {messageReceived}")
     except:
         print ("Failed to read message")
 
     #delay before sending next command
-    time.sleep(2)
+    time.sleep(0.1)
