6c79a8a4cae927d3507a670a374cae4d679db792
Martynas <karate.martynas1@gamil.com>
2023-05-21 21:39:33 +0200

    IT WORKS!!! :D

--- Python/ethernet_protocol/testingEthernet.py
+++ Python/ethernet_protocol/testingEthernet.py
@@ -1,15 +1,23 @@
-import socket
-import os
+from socket import *
+import time
 
-UDP_IP = "10.0.0.2"
-UDP_PORT = 8888
+HOST = '10.0.0.142'  # Standard loopback interface address (localhost)
+PORT = 8888  # Port to listen on (non-privileged ports are > 1023)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP,UDP_PORT))
-print("Socket connected")
+address= ('10.0.0.142', 8888) #define server IP and port
+clientSocket = socket(AF_INET, SOCK_DGRAM) #Set up the Socket
+clientSocket.settimeout(1) # Only wait 1 second for a response
+ 
+while(True):
+    data = bytes(69) #Set data request to Temperature
+    clientSocket.sendto(data, address) #Send the data request
+    try:
+        #Read response from arduino
+        rec_data, addr = clientSocket.recvfrom(2048) 
+        messageReceived = float(rec_data)
+        print ("Message: ", messageReceived)
+    except:
+        print ("ERROR!!!")
 
-while True:
-    data, addr = sock.recvfrom(500) # buffer size is 1024 bytes
-    print("received message: %s" % data)
-    
+    #delay before sending next command
+    time.sleep(2)
--- cpp/teensy/src/main.cpp
+++ cpp/teensy/src/main.cpp
@@ -34,14 +34,14 @@ Code written by: Vortex NTNU
 
 
 // Libraries for ethernet
-#include <Ethernet.h>
-#include <EthernetUdp.h>
+#include <NativeEthernet.h>
+#include <NativeEthernetUdp.h>
 
 // Variables that should be in .h file ==========
 // MAC address for every device must be different in every network
-byte macAddressTeensy[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+byte macAddressTeensy[] = {0xDE, 0xED, 0xBE, 0xEE, 0xFE, 0xED};
 // Necessary networking variables for Teensy to be part of the network
-IPAddress ipAddressTeensy(10, 0, 0, 1);
+IPAddress ipAddressTeensy(10, 0, 0, 142);
 unsigned int localPort = 8888;
 
 // Variables that should be in .cpp file ==========
@@ -57,9 +57,23 @@ void setup() {
         ;
     Serial.println("Serial connected\r\n");
 
+    // Initialize Ethernet pins
+    //Ethernet.init(20);
+
     // start the Ethernet
     Serial.println("Connecting to network");
     Ethernet.begin(macAddressTeensy, ipAddressTeensy);
+
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+        Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+        while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+        }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+        Serial.println("Ethernet cable is not connected.");
+    }
 
     // Check that the port is available
     if (Udp.begin(localPort)) {
@@ -67,12 +81,14 @@ void setup() {
     }
     else {
         Serial.println("FAILED could not connect to network");
+        Serial.println(Udp.begin(localPort));
     }
 }
 
 void loop() {
    // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
+  Serial.println(packetSize);
 
   if (packetSize) {
     // Get packet size
@@ -107,5 +123,5 @@ void loop() {
   }
 
   // Wait for 10 seconds
-  delay(10000);
+  delay(500);
 }
