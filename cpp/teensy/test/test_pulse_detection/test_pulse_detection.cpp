6cc1e3ee1fb9a41a287f98a4ca45f74ff7caa603
vortex <vortex.git@vortexntnu.no>
2024-03-19 19:52:51 +0100

    changed the repo filepaths

--- cpp/teensy/test/test_pulse_detection/test_pulse_detection.cpp
+++
@@ -1,7 +0,0 @@
-#include "test_pulse_determination.h"
-
-int main(int argc, char** argv) {
-    UNITY_BEGIN();
-    RUN_TEST(pulse_determination::test_short_time_fourier_transform);
-    UNITY_END();
-}
