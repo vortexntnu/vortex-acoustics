657f2fd36d5509cec648f33c31893701f4b955be
vortex <vortex.git@vortexntnu.no>
2024-03-22 06:44:14 +0100

    deleted duplicates

--- OldStuff/Python/multilateration/parameters.py
+++
@@ -1,9 +0,0 @@
-"""Contains parameters and constants used for the multilateration algorithm.
-"""
-
-from dataclasses import dataclass
-
-
-@dataclass
-class PhysicalConstants:
-    SOUND_SPEED: float = 1480.0
