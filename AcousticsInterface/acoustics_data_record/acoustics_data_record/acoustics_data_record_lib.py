9b64484d8ace1e3e92a813a9abf12591d435ed53
fnilsen1 <filipok04@gmail.com>
2025-04-14 18:33:17 +0200

    deleted unnecessary "readable csv" code

--- AcousticsInterface/acoustics_data_record/acoustics_data_record/acoustics_data_record_lib.py
+++ AcousticsInterface/acoustics_data_record/acoustics_data_record/acoustics_data_record_lib.py
@@ -76,20 +76,3 @@ class AcousticsDataRecordLib:
                     position,
                 ]
             )
-
-        #This made the code crash because of different array lengths...
-        # #Making another CSV that is more readable
-        #     df = pd.DataFrame({
-        #         'Hydrophone 1': hydrophone1,
-        #         'Hydrophone 2': hydrophone2,
-        #         'Hydrophone 3': hydrophone3,
-        #         'Hydrophone 4': hydrophone4,
-        #         'Hydrophone 5': hydrophone5
-        #     })
-
-        #     readable_dir = os.path.join(self.ros2package_directory, "readable_acoustics_data")
-        #     os.makedirs(readable_dir, exist_ok=True)
-
-        #     output_path = os.path.join(readable_dir, "readable_" + self.data_file_name)
-        #     df.to_csv(output_path, index=False)
-        #     print(f"📄 Readable CSV saved to: {output_path}")
